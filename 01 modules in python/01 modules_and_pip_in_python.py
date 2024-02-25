1a480459a7437cf128e2510578d2b42325aecf7f
MR-SAIRAM-7 <mr.sairamofficial@gmail.com>
2024-02-25 21:49:40 +0530

    modules and pip in python

---
+++ 01 modules in python/01 modules_and_pip_in_python.py
@@ -0,0 +1,19 @@
+# modules in python
+
+# in python a module is a file containing python code  that  can be  imported into another program. This allows you to reuse
+
+# the code modules consists of functions classes and variables that cn be utilized in other python files by importing them
+
+# for example there is module called math in python that provides functions for mathematical operations like square roots , trigonometric functions etc
+
+# pip in python
+
+# pip is the package installer for python . it stands for pip install packages or pip installs python
+    
+# pip is used to install and manage python packages which are bundles of code , calld modules
+
+# ex : -- pip install math
+# pip install flask => used in web dev
+
+# python is developed by guido van rossum
+# python is case sensitive languge
